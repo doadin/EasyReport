ab4d6fdc3f7102873b4414c92d8a4671762aaa5f
tbkizle <tbkizle@gmail.com>
2020-08-03 16:29:54 -0400

    Update to work on classic

--- core.lua
+++ core.lua
@@ -1,41 +1,91 @@
 EasyReport = LibStub("AceAddon-3.0"):NewAddon("EasyReport", "AceConsole-3.0")
 local er = EasyReport
 local AceGUI = LibStub("AceGUI-3.0")
+local isClassic = (WOW_PROJECT_ID == WOW_PROJECT_CLASSIC)
 
 local function ReportPlayerSpam()
-    C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_SPAM, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    if isClassic then
+        C_ChatInfo.ReportPlayer("spam", PlayerLocation:CreateFromUnit("target"))
+    end
+    if not isClassic then
+        C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_SPAM, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    end
 end
 
 local function ReportPlayerLanguage()
-    C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_LANGUAGE, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    if isClassic then
+        C_ChatInfo.ReportPlayer("language", PlayerLocation:CreateFromUnit("target"))
+    end
+    if not isClassic then
+        C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_LANGUAGE, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    end
 end
 
 local function ReportPlayerAbuse()
-    C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_ABUSE, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    if isClassic then
+        C_ChatInfo.ReportPlayer("abuse", PlayerLocation:CreateFromUnit("target"))
+    end
+    if not isClassic then
+        C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_ABUSE, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    end
 end
 
 local function ReportPlayerBadPlayerName()
-    C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_BAD_PLAYER_NAME, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    if isClassic then
+        C_ChatInfo.ReportPlayer("badplayername", PlayerLocation:CreateFromUnit("target"))
+    end
+    if not isClassic then
+        C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_BAD_PLAYER_NAME, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    end
 end
 
 local function ReportPlayerBadGuildName()
-    C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_BAD_GUILD_NAME, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    if isClassic then
+        C_ChatInfo.ReportPlayer("badguildname", PlayerLocation:CreateFromUnit("target"))
+    end
+    if not isClassic then
+        C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_BAD_GUILD_NAME, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    end
 end
 
 local function ReportPlayerCheating()
-    C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_CHEATING, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    if isClassic then
+        C_ChatInfo.ReportPlayer("cheating", PlayerLocation:CreateFromUnit("target"))
+    end
+    if not isClassic then
+        C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_CHEATING, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    end
 end
 
 local function ReportPlayerBadBattlePatName()
-    C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_BAD_BATTLEPET_NAME, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    if isClassic then
+        C_ChatInfo.ReportPlayer("badbattlepetname", PlayerLocation:CreateFromUnit("target"))
+    end
+    if not isClassic then
+        C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_BAD_BATTLEPET_NAME, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    end
 end
 
 local function ReportPlayerBadPetName()
-    C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_BAD_PET_NAME, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    if isClassic then
+        C_ChatInfo.ReportPlayer("badpetname", PlayerLocation:CreateFromUnit("target"))
+    end
+    if not isClassic then
+        C_ReportSystem.OpenReportPlayerDialog(PLAYER_REPORT_TYPE_BAD_PET_NAME, UnitName("target"), PlayerLocation:CreateFromUnit("target"))
+    end
 end
 
 local function ReportServerLag()
-    C_ReportSystem.ReportServerLag()
+    if isClassic then
+        C_ChatInfo.ReportServerLag()
+    end
+    if not isClassic then
+        C_ReportSystem.ReportServerLag()
+    end
+end
+local function close(widget)
+    --AceGUI:Release(widget)
+    print("clsoe clicked")
 end
 
 --local options = {}
@@ -51,12 +101,26 @@ function er:ChatCommand(input)
   if not input or input:trim() == "" then
     --LibStub("AceConfigDialog-3.0"):Open("MyOptions")
     -- Create a container frame
+    if _G["EasyReportFrame"] then
+        --print("Frame already Exists")
+        return
+    end
     local f = AceGUI:Create("Frame")
-    f:SetCallback("OnClose",function(widget) AceGUI:Release(widget) end)
+    -- Add the frame as a global variable under the name `MyGlobalFrameName`
+    _G["EasyReportFrame"] = f.frame
+    -- Register the global variable `MyGlobalFrameName` as a "special frame"
+    -- so that it is closed when the escape key is pressed.
+    tinsert(UISpecialFrames, "EasyReportFrame")
+
+    f:SetCallback("OnClose",function(widget) AceGUI:Release(widget) ; _G["EasyReportFrame"] = nil end)
     f:SetTitle("Easy Report")
     f:SetWidth(200)
-    f:SetHeight(260)
+    f:SetHeight(290)
     f:SetLayout("List")
+    if EasyReportFramePossition then
+        print("Restoring possition")
+        f:SetPoint(EasyReportFramePossition)
+    end
     -- Create a button
     local btn1 = AceGUI:Create("Button")
     btn1:SetWidth(165)
@@ -117,9 +181,17 @@ function er:ChatCommand(input)
     local btn9 = AceGUI:Create("Button")
     btn9:SetWidth(165)
     btn9:SetText("Report Server Lag")
-    btn9:SetCallback("OnClick", ReportPlayerBadPetName)
+    btn9:SetCallback("OnClick", ReportServerLag)
     -- Add the button to the container
     f:AddChild(btn9)
+
+    -- Create a button
+    local btn10 = AceGUI:Create("Button")
+    btn10:SetWidth(165)
+    btn10:SetText("Close")
+    btn10:SetCallback("OnClick", close)
+    -- Add the button to the container
+    --f:AddChild(btn10)
 
     f:PauseLayout()
   end
@@ -165,10 +237,42 @@ function er:OnDisable()
     -- Called when the addon is disabled
 end
 
---function er:REPORT_PLAYER_RESULT()
---    local success = success 
---    print("Report Made", success)
---end
---
-----Events
---er:RegisterEvent("REPORT_PLAYER_RESULT")
+local function OnEvent(self, event, ...)
+    if isClassic then
+        print("Report Made")
+    end
+    if ... == true then
+        print("Report Made")
+    end
+    if ... == false then
+        print("Report Failed")
+    end
+    if event == "GLOBAL_MOUSE_DOWN" then
+        --print(_G["EasyReportFrame"]:GetPoint())
+    end
+    if event == "GLOBAL_MOUSE_UP" then
+        --print(_G["EasyReportFrame"]:GetPoint())
+        if _G["EasyReportFrame"] then
+            --print("saveing possition")
+            --point, relativeTo, relativePoint, xOfs, yOfs = _G["EasyReportFrame"]:GetPoint()
+            --EasyReportFramePossition = point .. ", " .. "nil" .. ", " .. relativePoint .. ", " .. xOfs .. ", " .. yOfs
+            --EasyReportFramePossition = point .. ", " .. " " .. ", " .. relativePoint
+            EasyReportFramePossition = _G["EasyReportFrame"]:GetPoint()
+            --print(EasyReportFramePossition)
+        end
+    end
+end
+
+--[===[@non-retail@
+local EventFrame = CreateFrame("Frame")
+EventFrame:RegisterEvent("PLAYER_REPORT_SUBMITTED")
+EventFrame:SetScript("OnEvent", OnEvent)
+--@end-non-retail@]===]
+
+--@retail@
+local EventFrame = CreateFrame("Frame")
+EventFrame:RegisterEvent("REPORT_PLAYER_RESULT")
+EventFrame:RegisterEvent("GLOBAL_MOUSE_DOWN")
+EventFrame:RegisterEvent("GLOBAL_MOUSE_UP")
+EventFrame:SetScript("OnEvent", OnEvent)
+--@end-retail@
